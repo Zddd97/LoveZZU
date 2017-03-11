0f799986cf23953c86424de7ba8d6e841b8e4182
zhao <zhao199606@gamil.com>
2017-03-11 20:34:05 +0800

    校园中间滑动

--- app/src/main/java/com/gjf/lovezzu/fragment/School_shoolfragment.java
+++ app/src/main/java/com/gjf/lovezzu/fragment/School_shoolfragment.java
@@ -1,8 +1,11 @@
 package com.gjf.lovezzu.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +15,13 @@ import com.gc.flashview.FlashView;
 import com.gc.flashview.constants.EffectConstants;
 import com.gc.flashview.listener.FlashViewListener;
 import com.gjf.lovezzu.R;
+import com.gjf.lovezzu.SchoolMid;
+import com.gjf.lovezzu.SchoolMidAdapter;
+import com.gjf.lovezzu.activity.MainActivity;
+
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by lenovo047 on 2017/3/9.
@@ -23,6 +31,7 @@ public class School_shoolfragment extends android.app.Fragment {
 
     private FlashView flashView;
     private ArrayList<String> imageUrls;
+    private List<SchoolMid> schoolMidList = new ArrayList<>();
 
     @Nullable
     @Override
@@ -30,11 +39,8 @@ public class School_shoolfragment extends android.app.Fragment {
         final View view = inflater.inflate(R.layout.inchool_school_view, container, false);
         flashView = (FlashView) view.findViewById(R.id.flash_view);
         imageUrls = new ArrayList<String>();
-        //imageUrls.add("http://www.qipaox.com/tupian/200810/20081051924582.jpg");
         imageUrls.add("http://www.bz55.com/uploads1/allimg/120312/1_120312100435_8.jpg");
-        //imageUrls.add("http://img3.iqilu.com/data/attachment/forum/201308/21/192654ai88zf6zaa60zddo.jpg");
         imageUrls.add("http://img2.pconline.com.cn/pconline/0706/19/1038447_34.jpg");
-        //imageUrls.add("http://www.kole8.com/desktop/desk_file-11/2/2/2012/11/2012113013552959.jpg");
         imageUrls.add("http://www.237.cc/uploads/pcline/712_0_1680x1050.jpg");
         flashView.setImageUris(imageUrls);
         flashView.setEffect(EffectConstants.DEFAULT_EFFECT);//更改图片切换的动画效果
@@ -46,10 +52,32 @@ public class School_shoolfragment extends android.app.Fragment {
 
             }
         });
+
+        //中间部分
+        initSchoolMidList();
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.in_mid_school);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(view.getContext());
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerView.setLayoutManager(layoutManager);
+        SchoolMidAdapter adapter = new SchoolMidAdapter(schoolMidList);
+        recyclerView.setAdapter(adapter);
+
+
         return view;
     }
 
+    private void initSchoolMidList() {
+        Intent intent = new Intent(getContext(),MainActivity.class);
+        SchoolMid schoolMid_1 = new SchoolMid(R.drawable.schoolintroduce, intent);
+        SchoolMid schoolMid_2 = new SchoolMid(R.drawable.schoolstyle, intent);
+        SchoolMid schoolMid_3 = new SchoolMid(R.drawable.schoolfamous, intent);
+        SchoolMid schoolMid_4 = new SchoolMid(R.drawable.schoolnews, intent);
 
+        schoolMidList.add(schoolMid_1);
+        schoolMidList.add(schoolMid_2);
+        schoolMidList.add(schoolMid_3);
+        schoolMidList.add(schoolMid_4);
+    }
 
      /*
         * 设置向服务器请求图片--返回jison--{json:图片的URL和点击图片跳转的的URL}（有格式）--本地解析json--
