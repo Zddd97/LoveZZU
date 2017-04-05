6980f03ba3f3cdce29ff3fd1ece5eacbe52b96f6
G <gjfwyfcm@gmail.com>
2017-04-05 17:31:10 +0800

    1
    1

---
+++ app/src/main/java/com/gjf/lovezzu/activity/TaoyuActivity.java
@@ -0,0 +1,46 @@
+package com.gjf.lovezzu.activity;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.gjf.lovezzu.R;
+import com.gjf.lovezzu.fragment.TaoyuFragment;
+
+/**
+ * Created by lenovo047 on 2017/3/25.
+ */
+
+public class TaoyuActivity extends AppCompatActivity{
+    private TaoyuFragment TaoyuFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);////
+        setContentView(R.layout.taoyu_activity);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+        setDefaultFragment();
+
+    }
+
+    private void setDefaultFragment() {
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        TaoyuFragment = new TaoyuFragment();
+        transaction.replace(R.id.id_taoyuactivity, TaoyuFragment);
+        transaction.commit();
+    }
+}
+
+
