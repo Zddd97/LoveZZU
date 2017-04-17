010d5171780f6e1add1414efd58d81ed17148c63
G <gjfwyfcm@gmail.com>
2017-04-17 18:57:27 +0800

    2017/04/13 1、掏鱼显示商品列表的网络操作完成。但有未知bug存在。 2、一起玩界面已经完成，bug已经修复。 3、下一步将进行掏鱼的搜索以及分类的网络操作。 4、将进行表白墙界面的显示以及网络操作。 2017/04/12 1、掏鱼界面开始 2、修复bug
    2017/04/09
    1、学校首页与服务端进行了对接，基本完善，二级界面正在开发。
    2、更新了淘鱼界面的代码以及网络操作所需要的实体类。
    3、完善了新闻主界面直接弹出输入法的行为，但未解决怎样使输入框在输入法的上方。
    4、一起玩界面正在进行，相关代码已经上传，但未实现功能。

--- app/src/main/java/com/gjf/lovezzu/view/TaoyuAdapter.java
+++ app/src/main/java/com/gjf/lovezzu/view/TaoyuAdapter.java
@@ -19,25 +19,26 @@ import java.util.List;
  * Created by lenovo047 on 2017/3/24.
  */
 
-
 public class TaoyuAdapter extends RecyclerView.Adapter<TaoyuAdapter.ViewHolder>{
     private List<TaoyuDataBridging> taoyuResultList;
     private  Context context;
     private TaoyuDataBridging taoyuResult;
+    private LayoutInflater inflater;
 
     public TaoyuAdapter(List<TaoyuDataBridging> taoyuResultList, Context context) {
         this.taoyuResultList = taoyuResultList;
         this.context = context;
+        inflater = LayoutInflater.from(context);
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.taoyu, parent, false);
-        final ViewHolder holder = new ViewHolder(view);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.taoyu,parent,false);
+        final  ViewHolder holder =new ViewHolder(view);
         holder.taoyuView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(v.getContext(), "点击事件", Toast.LENGTH_SHORT).show();
+                Toast.makeText(v.getContext(),"点击事件",Toast.LENGTH_SHORT).show();
             }
         });
         return holder;
@@ -54,13 +55,13 @@ public class TaoyuAdapter extends RecyclerView.Adapter<TaoyuAdapter.ViewHolder>{
                 .error(R.drawable.__picker_ic_broken_image_black_48dp)
                 .into(holder.taoyuimag);
         //holder.taoyuimag.setImageResource(taoyuResult.getImageID2());
-       // holder.title.setText(taoyuResult.getGoods().getGdescribe());
-//        holder.comment.setText(taoyuResult.getComment());
-//        holder.position.setText(taoyuResult.getPosition());
-//        holder.price.setText(taoyuResult.getPrice());
-       //holder.phone.setText(taoyuResult.getUserinfo().getPhone());
-//        holder.time.setText(taoyuResult.getTime());
-//        holder.zan.setText(taoyuResult.getZan());
+
+       holder.comment.setText(taoyuResult.getGoods().getGname());
+       holder.position.setText(taoyuResult.getUserinfo().getAcademy());
+       holder.price.setText(taoyuResult.getGoods().getGprice());
+       holder.phone.setText(taoyuResult.getUserinfo().getPhone());
+       holder.time.setText(taoyuResult.getGoods().getGdate());
+        holder.zan.setText(taoyuResult.getGoods().getGname());
 
 
     }
@@ -73,27 +74,28 @@ public class TaoyuAdapter extends RecyclerView.Adapter<TaoyuAdapter.ViewHolder>{
     public class ViewHolder extends RecyclerView.ViewHolder {
         View taoyuView;
         ImageView touxiang;
-        ImageView taoyuimag;
+        ImageView  taoyuimag;
         TextView phone;
         TextView time;
         TextView price;
         TextView position;
         TextView zan;
         TextView comment;
-        TextView title;
+
 
         public ViewHolder(View itemView) {
             super(itemView);
             taoyuView = itemView;
             taoyuimag = (ImageView) itemView.findViewById(R.id.taoyuimag);
             touxiang = (ImageView) itemView.findViewById(R.id.taoyuimag);
-            phone = (TextView) itemView.findViewById(R.id.phone);
-            time = (TextView) itemView.findViewById(R.id.time);
+            phone =(TextView) itemView.findViewById(R.id.phone);
+            time  =(TextView) itemView.findViewById(R.id.time);
             price = (TextView) itemView.findViewById(R.id.price);
             position = (TextView) itemView.findViewById(R.id.position);
-            zan = (TextView) itemView.findViewById(R.id.zan);
+            zan = (TextView)  itemView.findViewById(R.id.zan);
             comment = (TextView) itemView.findViewById(R.id.comment);
-            title = (TextView) itemView.findViewById(R.id.title);
+
+
 
 
         }
